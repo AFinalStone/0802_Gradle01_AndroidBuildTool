fb1139972204e4aa7ca88e0dcc0a423e316261c5
afinalstone <afinalstone@foxmail.com>
2022-05-17 00:17:42 +0800

    解决编译错误

--- plugin-example/src/main/groovy/com/afs/example/plugin/MyPlugin.groovy
+++ plugin-example/src/main/groovy/com/afs/example/plugin/MyPlugin.groovy
@@ -4,7 +4,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class MyPlugin implements Plugin<Project> {
-    @Override
     void apply(Project project) {
         println("执行自定义插件");
         project.task("haha") {
